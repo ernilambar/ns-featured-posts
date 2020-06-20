e46f46c11291cea2ce8a1862498ed15813156133
Nilambar Sharma <nilambar@outlook.com>
2020-06-20 15:56:55 +0545

    Fix main class

--- class-ns-featured-posts.php
+++ class-ns-featured-posts.php
@@ -2,327 +2,295 @@
 /**
  * NS Featured Posts
  *
- * @package   NS_Featured_Posts
- * @author    Nilambar Sharma <nilambar@outlook.com>
- * @license   GPL-2.0+
- * @link      https://www.nilambar.net
- * @copyright 2013 Nilambar Sharma
+ * @package NS_Featured_Posts
  */
 
 /**
  * NS Featured Posts Plugin class.
  *
- * @package NS_Featured_Posts
- * @author  Nilambar Sharma <nilambar@outlook.com>
+ * @since 1.0.0
  */
-class NS_Featured_Posts
-{
-    /**
-     * Plugin version, used for cache-busting of style and script file references.
-     *
-     * @since   1.0.0
-     *
-     * @var     string
-     */
+class NS_Featured_Posts {
 
-    const VERSION = '1.4.2';
+	/**
+	 * Plugin version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	const VERSION = '1.4.2';
 
-    /**
-     * Unique identifier for your plugin.
-     *
-     * The variable name is used as the text domain when internationalizing strings
-     * of text. Its value should match the Text Domain file header in the main
-     * plugin file.
-     *
-     * @since    1.0.0
-     *
-     * @var      string
-     */
-    protected $plugin_slug = 'ns-featured-posts';
+	/**
+	 * Plugin slug.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	protected $plugin_slug = 'ns-featured-posts';
 
-    /**
-     * Plugin default options
-     *
-     * Default options for the plugin.
-     *
-     * @since    1.0.0
-     *
-     * @var      array
-     */
-    protected static $default_options = null ;
+	/**
+	 * Plugin default options.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var array
+	 */
+	protected static $default_options = null;
 
+	/**
+	 * Plugin options.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var array
+	 */
 	protected $options = array();
 
-    /**
-     * Instance of this class.
-     *
-     * @since    1.0.0
-     *
-     * @var      object
-     */
-    protected static $instance = null;
+	/**
+	 * Instance of this class.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var object
+	 */
+	protected static $instance = null;
 
-    /**
-     * Initialize the plugin by setting localization and loading public scripts
-     * and styles.
-     *
-     * @since     1.0.0
-     */
-    private function __construct()
-    {
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.0.0
+	 */
+	private function __construct() {
+		// Load plugin text domain.
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
 
-        // Load plugin text domain.
-        add_action('init', array($this, 'load_plugin_textdomain'));
+		// Activate plugin when new blog is added.
+		add_action( 'wpmu_new_blog', array( $this, 'activate_new_site' ) );
 
-        // Activate plugin when new blog is added.
-        add_action('wpmu_new_blog', array($this, 'activate_new_site'));
+		self::$default_options = array(
+			'nsfp_posttypes' => array( 'post' ),
+		);
 
-        self :: $default_options = array(
-            'nsfp_posttypes' => array( 'post' => 1 ),
-        );
-
-		$this -> _setDefaultOptions();
+		$this->set_default_options();
 
 		// Get current options.
-        $this->_getCurrentOptions();
+		$this->get_current_options();
 
-        // Migrate options.
-        add_action( 'init', array( $this, 'migrate_options' ) );
-    }
+		// Migrate options.
+		add_action( 'init', array( $this, 'migrate_options' ) );
+	}
 
-    /**
-     * Return the plugin slug.
-     *
-     * @since    1.0.0
-     *
-     * @return    Plugin slug variable.
-     */
-    public function get_plugin_slug()
-    {
-        return $this->plugin_slug;
-    }
+	/**
+	 * Return the plugin slug.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return Plugin slug.
+	 */
+	public function get_plugin_slug() {
+		return $this->plugin_slug;
+	}
 
-    /**
-     * Return an instance of this class.
-     *
-     * @since     1.0.0
-     *
-     * @return    object    A single instance of this class.
-     */
-    public static function get_instance()
-    {
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return object A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
 
-        // If the single instance hasn't been set, set it now.
-        if (null == self::$instance)
-        {
-            self::$instance = new self;
-        }
+		return self::$instance;
+	}
 
-        return self::$instance;
-    }
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param boolean $network_wide Whether network wide.
+	 */
+	public static function activate( $network_wide ) {
 
-    /**
-     * Fired when the plugin is activated.
-     *
-     * @since    1.0.0
-     *
-     * @param    boolean    $network_wide    True if WPMU superadmin uses
-     *                                       "Network Activate" action, false if
-     *                                       WPMU is disabled or plugin is
-     *                                       activated on an individual blog.
-     */
-    public static function activate($network_wide)
-    {
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
 
-        if (function_exists('is_multisite') && is_multisite())
-        {
+			if ( $network_wide ) {
 
-            if ($network_wide)
-            {
+				// Get all blog ids.
+				$blog_ids = self::get_blog_ids();
 
-                // Get all blog ids
-                $blog_ids = self::get_blog_ids();
+				foreach ( $blog_ids as $blog_id ) {
 
-                foreach ($blog_ids as $blog_id)
-                {
+					switch_to_blog( $blog_id );
+					self::single_activate();
+				}
 
-                    switch_to_blog($blog_id);
-                    self::single_activate();
-                }
+				restore_current_blog();
+			} else {
+				self::single_activate();
+			}
+		} else {
+			self::single_activate();
+		}
+	}
 
-                restore_current_blog();
-            }
-            else
-            {
-                self::single_activate();
-            }
-        }
-        else
-        {
-            self::single_activate();
-        }
-    }
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param boolean $network_wide Whether network wide.
+	 */
+	public static function deactivate( $network_wide ) {
 
-    /**
-     * Fired when the plugin is deactivated.
-     *
-     * @since    1.0.0
-     *
-     * @param    boolean    $network_wide    True if WPMU superadmin uses
-     *                                       "Network Deactivate" action, false if
-     *                                       WPMU is disabled or plugin is
-     *                                       deactivated on an individual blog.
-     */
-    public static function deactivate($network_wide)
-    {
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
 
-        if (function_exists('is_multisite') && is_multisite())
-        {
+			if ( $network_wide ) {
 
-            if ($network_wide)
-            {
+				// Get all blog ids.
+				$blog_ids = self::get_blog_ids();
 
-                // Get all blog ids
-                $blog_ids = self::get_blog_ids();
+				foreach ( $blog_ids as $blog_id ) {
 
-                foreach ($blog_ids as $blog_id)
-                {
+					switch_to_blog( $blog_id );
+					self::single_deactivate();
+				}
 
-                    switch_to_blog($blog_id);
-                    self::single_deactivate();
-                }
+				restore_current_blog();
+			} else {
+				self::single_deactivate();
+			}
+		} else {
+			self::single_deactivate();
+		}
+	}
 
-                restore_current_blog();
-            }
-            else
-            {
-                self::single_deactivate();
-            }
-        }
-        else
-        {
-            self::single_deactivate();
-        }
-    }
+	/**
+	 * Fired when a new site is activated with a multisite environment.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $blog_id ID of the new blog.
+	 */
+	public function activate_new_site( $blog_id ) {
+		if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
+			return;
+		}
 
-    /**
-     * Fired when a new site is activated with a WPMU environment.
-     *
-     * @since    1.0.0
-     *
-     * @param    int    $blog_id    ID of the new blog.
-     */
-    public function activate_new_site($blog_id)
-    {
+		switch_to_blog( $blog_id );
+		self::single_activate();
+		restore_current_blog();
+	}
 
-        if (1 !== did_action('wpmu_new_blog'))
-        {
-            return;
-        }
+	/**
+	 * Get all active blog ids.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array|false The blog ids, false if no matches.
+	 */
+	private static function get_blog_ids() {
+		global $wpdb;
 
-        switch_to_blog($blog_id);
-        self::single_activate();
-        restore_current_blog();
-    }
+		$ids = array();
 
-    /**
-     * Get all blog ids of blogs in the current network that are:
-     * - not archived
-     * - not spam
-     * - not deleted
-     *
-     * @since    1.0.0
-     *
-     * @return   array|false    The blog ids, false if no matches.
-     */
-    private static function get_blog_ids()
-    {
+		$output = $wpdb->get_results( "SELECT blog_id FROM $wpdb->blogs WHERE archived = '0' AND spam = '0' AND deleted = '0'", ARRAY_A );
 
-        global $wpdb;
+		if ( $output ) {
+			$ids = wp_list_pluck( $output, 'blog_id' );
+		}
 
-        // get an array of blog ids
-        $sql = "SELECT blog_id FROM $wpdb->blogs
-            WHERE archived = '0' AND spam = '0'
-            AND deleted = '0'";
+		return $ids;
+	}
 
-        return $wpdb->get_col($sql);
-    }
+	/**
+	 * Fired for each blog when the plugin is activated.
+	 *
+	 * @since    1.0.0
+	 */
+	private static function single_activate() {
+		$option_name = 'nsfp_plugin_options';
+		update_option( $option_name, self::$default_options );
+	}
 
-    /**
-     * Fired for each blog when the plugin is activated.
-     *
-     * @since    1.0.0
-     */
-    private static function single_activate()
-    {
-        // Define activation functionality here
-        $option_name = 'nsfp_plugin_options';
-        update_option($option_name, self :: $default_options);
-    }
+	/**
+	 * Fired for each blog when the plugin is deactivated.
+	 *
+	 * @since 1.0.0
+	 */
+	private static function single_deactivate() {
+	}
 
-    /**
-     * Fired for each blog when the plugin is deactivated.
-     *
-     * @since    1.0.0
-     */
-    private static function single_deactivate()
-    {
-        // Define deactivation functionality here
-        $option_name = 'nsfp_plugin_options';
-        // delete_option($option_name);
-    }
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since 1.0.0
+	 */
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain( $this->plugin_slug );
+	}
 
-    /**
-     * Load the plugin text domain for translation.
-     *
-     * @since    1.0.0
-     */
-    public function load_plugin_textdomain() {
+	/**
+	 * Fetch plugin options.
+	 *
+	 * @since 1.0.0
+	 */
+	private function get_current_options() {
+		$nsfp_options  = array_merge( self::$default_options, (array) get_option( 'nsfp_plugin_options', array() ) );
+		$this->options = $nsfp_options;
+	}
 
-        load_plugin_textdomain( $this->plugin_slug );
-    }
+	/**
+	 * Set default plugin options.
+	 *
+	 * @since 1.0.0
+	 */
+	private function set_default_options() {
+		if ( ! get_option( 'nsfp_plugin_options' ) ) {
+			update_option( 'nsfp_plugin_options', self::$default_options );
+		}
+	}
 
-	private function _getCurrentOptions()
-    {
-		$nsfp_options = array_merge( self :: $default_options , (array) get_option( 'nsfp_plugin_options', array() ) );
-        $this->options = $nsfp_options;
-    }
-
-	//get default options and saves in options table
-    private function _setDefaultOptions()
-    {
-        if( !get_option( 'nsfp_plugin_options' ) ) {
-            update_option('nsfp_plugin_options', self :: $default_options);
-        }
-    }
-
-	private function _removePluginOptions()
-    {
-        delete_option('nsfp_plugin_options');
-    }
-
-	public function ns_featured_posts_get_options_array(){
+	/**
+	 * Get plugin options details.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array Options array.
+	 */
+	public function ns_featured_posts_get_options_array() {
 		return $this->options;
 	}
 
-    public function migrate_options() {
-    	if ( 'yes' === get_option( 'nsfp_option_migration_complete' ) ) {
-    		return;
-    	}
+	/**
+	 * Migrate options.
+	 *
+	 * @since 1.4.3
+	 */
+	public function migrate_options() {
+		if ( 'yes' === get_option( 'nsfp_option_migration_complete' ) ) {
+			return;
+		}
 
-    	$opt = get_option( 'nsfp_plugin_options' );
+		$opt = get_option( 'nsfp_plugin_options' );
 
-    	if ( $opt ) {
-    		if ( isset( $opt['nsfp_posttypes'] ) && ! empty( $opt['nsfp_posttypes'] ) ) {
+		if ( $opt ) {
+			if ( isset( $opt['nsfp_posttypes'] ) && ! empty( $opt['nsfp_posttypes'] ) ) {
 
-	    		$values = array_keys( $opt['nsfp_posttypes'] );
+				$values = array_keys( $opt['nsfp_posttypes'] );
 
-	    		$opt['nsfp_posttypes'] = $values;
+				$opt['nsfp_posttypes'] = $values;
 
-	    		update_option( 'nsfp_plugin_options', $opt );
-    		}
-    	}
+				update_option( 'nsfp_plugin_options', $opt );
+			}
+		}
 
-    	update_option( 'nsfp_option_migration_complete', 'yes' );
-    }
+		update_option( 'nsfp_option_migration_complete', 'yes' );
+	}
 }
