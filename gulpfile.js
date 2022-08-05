abea67b0e2d6eb08d4fb50a4edf91361c099f8f2
Nilambar Sharma <nilambar@outlook.com>
2022-08-05 21:50:15 +0545

    Refine settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // Env.
-require('dotenv').config()
+require('dotenv').config();
 
 // Config.
 var rootPath = './';
@@ -7,59 +7,20 @@ var rootPath = './';
 // Gulp.
 var gulp = require('gulp');
 
-// Zip.
-var zip = require('gulp-zip');
-
-// File system.
-var fs = require('fs');
-
-// Package.
-var pkg = JSON.parse(fs.readFileSync('./package.json'));
-
-// Delete.
-var del = require('del');
-
 // Browser sync.
 var browserSync = require('browser-sync').create();
-
-// Deploy files list.
-var deploy_files_list = [
-	'assets/**',
-	'includes/**',
-	'languages/**',
-	'vendor/**',
-	'readme.txt',
-	pkg.main_file
-];
 
 // Watch.
 gulp.task('watch', function() {
     browserSync.init({
-        proxy: process.env.DEV_SERVER_URL,
-        open: true
+			proxy: process.env.DEV_SERVER_URL,
+			open: true
     });
 
-    // Watch CSS files.
-    gulp.watch(rootPath + '**/**/*.css').on('change',browserSync.reload);
-
-    // Watch PHP files.
-    gulp.watch(rootPath + '**/**/*.php').on('change',browserSync.reload);
-});
-
-// Clean deploy folder.
-gulp.task('clean:deploy', function() {
-    return del('deploy')
-});
-
-// Copy to deploy folder.
-gulp.task('copy:deploy', function() {
-	return gulp.src(deploy_files_list, {base: '.'})
-	    .pipe(gulp.dest('deploy/' + pkg.name))
-	    .pipe(zip(pkg.name + '.zip'))
-	    .pipe(gulp.dest('deploy'))
+    gulp.watch(rootPath + 'assets/**/*.css').on('change', browserSync.reload);
+    gulp.watch(rootPath + 'assets/**/*.js').on('change', browserSync.reload);
+    gulp.watch(rootPath + '**/**/*.php').on('change', browserSync.reload);
 });
 
 // Tasks.
 gulp.task( 'default', gulp.series('watch'));
-
-gulp.task( 'deploy', gulp.series('clean:deploy', 'copy:deploy'));
