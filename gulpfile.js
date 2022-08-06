d1eebc29101f199aad9f440b2dc95694e9bf1fa0
Nilambar Sharma <nilambar@outlook.com>
2022-08-06 08:49:46 +0545

    Implemnt eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
 // Env.
-require('dotenv').config();
+require( 'dotenv' ).config();
 
 // Config.
 var rootPath = './';
 
 // Gulp.
-var gulp = require('gulp');
+var gulp = require( 'gulp' );
 
 // Browser sync.
-var browserSync = require('browser-sync').create();
+var browserSync = require( 'browser-sync' ).create();
 
 // Watch.
-gulp.task('watch', function() {
-    browserSync.init({
-			proxy: process.env.DEV_SERVER_URL,
-			open: true
-    });
+gulp.task( 'watch', function() {
+	browserSync.init( {
+		proxy: process.env.DEV_SERVER_URL,
+		open: true,
+	} );
 
-    gulp.watch(rootPath + 'assets/**/*.css').on('change', browserSync.reload);
-    gulp.watch(rootPath + 'assets/**/*.js').on('change', browserSync.reload);
-    gulp.watch(rootPath + '**/**/*.php').on('change', browserSync.reload);
-});
+	gulp.watch( rootPath + 'assets/**/*.css' ).on( 'change', browserSync.reload );
+	gulp.watch( rootPath + 'assets/**/*.js' ).on( 'change', browserSync.reload );
+	gulp.watch( rootPath + '**/**/*.php' ).on( 'change', browserSync.reload );
+} );
 
 // Tasks.
-gulp.task( 'default', gulp.series('watch'));
+gulp.task( 'default', gulp.series( 'watch' ) );
